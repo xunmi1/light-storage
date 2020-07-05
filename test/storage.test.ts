344b2edf91b3743ee39e92e3d6c88f94e18fd660
xunmi <xunmi1@outlook.com>
2020-07-05 11:51:00 +0800

    test: update test cases

--- test/storage.test.ts
+++ test/storage.test.ts
@@ -3,57 +3,19 @@ import LightStorage from '../src';
 const delay = (timeout: number) => new Promise(resolve => setTimeout(resolve, timeout));
 
 describe('create', () => {
-  const prefix = 'mock-test';
-  const storage = new LightStorage(prefix);
+  const defaultPrefix = 'light-storage';
+  const storage = new LightStorage();
 
   test('created instance', () => {
     expect(storage).toStrictEqual(expect.any(LightStorage));
-    expect(storage.prefix).toBe(prefix);
+    expect(storage.prefix).toBe(defaultPrefix);
   });
 
   test('change prefix', () => {
-    const newPrefix = 'mock-test-new';
+    const newPrefix = 'change prefix';
     storage.prefix = newPrefix;
     expect(storage.prefix).toBe(newPrefix);
   });
-});
-
-describe('get data', () => {
-  const key = 'origin value';
-  const fullKey = `light-storage-${key}`;
-  const origin = JSON.stringify({ value: true });
-  window.localStorage.setItem(key, origin);
-  window.localStorage.setItem(fullKey, origin);
-  const db = new LightStorage();
-
-  test('get origin value', () => {
-    expect(db.get(key)).toBe(true);
-    expect(db.get(fullKey)).toBe(true);
-  });
-
-  test('get default value', () => {
-    const key = 'get data';
-    const defaultValue = { value: true, label: '获取默认值' };
-    expect(db.get(key, defaultValue)).toEqual(defaultValue);
-  });
-
-  test('use native removeItem', () => {
-    window.localStorage.removeItem(fullKey);
-    expect(db.get(key)).toBeUndefined();
-  })
-
-  test('get unsafe data', () => {
-    const unsafeData = '[{];<>';
-    window.localStorage.setItem(fullKey, unsafeData);
-    expect(db.get(key)).toBe(unsafeData);
-  })
-
-  test('get number-like', () => {
-    const numberLike = '123456';
-    window.localStorage.setItem(fullKey, numberLike);
-    expect(db.get(key)).toBe(Number(numberLike));
-  })
-
 });
 
 /**
@@ -62,13 +24,42 @@ describe('get data', () => {
 const PREFIX = 'test';
 const instance = new LightStorage(PREFIX);
 
-afterEach(() => instance.clear());
+afterEach(() => window.localStorage.clear());
 
-describe('has value', () => {
-  const key = 'undefined';
-  expect(instance.has(key)).toBe(false);
-  instance.set(key, key);
-  expect(instance.has(key)).toBe(true);
+describe('get data', () => {
+  const key = 'origin value';
+  const fullKey = `${PREFIX}-${key}`;
+  const origin = JSON.stringify({ value: true });
+
+  test('get origin value', () => {
+    window.localStorage.setItem(fullKey, origin);
+    expect(instance.get(key)).toBe(true);
+    expect(instance.get(fullKey)).toBe(true);
+  });
+
+  test('get default value', () => {
+    const key = 'get data';
+    const defaultValue = { value: true, label: '获取默认值' };
+    expect(instance.get(key, defaultValue)).toEqual(defaultValue);
+  });
+
+  test('use native removeItem', () => {
+    window.localStorage.removeItem(fullKey);
+    expect(instance.get(key)).toBeUndefined();
+  })
+
+  test('get unsafe data', () => {
+    const unsafeData = '[{];<>';
+    window.localStorage.setItem(fullKey, unsafeData);
+    expect(instance.get(key)).toBe(unsafeData);
+  })
+
+  test('get number-like', () => {
+    const numberLike = '123456';
+    window.localStorage.setItem(fullKey, numberLike);
+    expect(instance.get(key)).toBe(Number(numberLike));
+  })
+
 });
 
 describe('check data', () => {
@@ -99,7 +90,7 @@ describe('check validity period', () => {
     const key = 'base';
     instance.set(key, mockData, 10);
     expect(instance.get(key)).toBe(mockData);
-    await delay(11);
+    await delay(15);
     expect(instance.get(key)).toBeUndefined();
   });
 
@@ -128,23 +119,35 @@ describe('check validity period', () => {
   });
 });
 
+describe('has value', () => {
+  test('not maxAge', () => {
+    const key = 'not maxAge';
+    expect(instance.has(key)).toBe(false);
+    instance.set(key, '');
+    expect(instance.has(key)).toBe(true);
+  })
+
+  test('has maxAge', async () => {
+    instance.set('maxAge', '', 5);
+    await delay(10);
+    expect(instance.has('maxAge')).toBe(false);
+  })
+});
 
 describe('remove one', () => {
   test('remove one', () => {
     instance.set('mock', true, 50);
-    instance.set('mock-copy', true);
     expect(instance.remove('mock')).toBe(true);
     expect(instance.get('mock')).toBeUndefined();
-    expect(instance.get('mock-copy')).toBeTruthy();
   });
 
   test('remove undefined key', () => {
     expect(instance.remove('undefined-mock')).toBe(false);
   });
-
 });
 
 describe('cleanup', () => {
+  instance.set('mock', true);
   instance.clear();
   const keys = Object.keys(window.localStorage).filter(k => k.startsWith(PREFIX))
   expect(keys.length).toBe(0);
@@ -153,20 +156,41 @@ describe('cleanup', () => {
 describe('verify size', () => {
   beforeAll(() => {
     instance.clear();
-    instance.set('verify size', '');
   })
 
+  test('after cleanup', () => {
+    expect(instance.size).toBe(0);
+  });
+
   test('size is one', () => {
+    instance.set('verify size', '');
     expect(instance.size).toBe(1);
   });
 
   test('use native removeItem', () => {
-    window.localStorage.removeItem('verify size');
+    instance.set('verify size', '');
+    window.localStorage.removeItem(`${PREFIX}-verify size`);
     expect(instance.size).toBe(0);
   });
 
   test('size of new instance', () => {
-    const db = new LightStorage('new-instance-size');
-    expect(db.size).toBe(0);
+    const prefix = 'new-instance-size';
+    window.localStorage.setItem(`${prefix}-a`, 'a');
+    const db = new LightStorage(prefix);
+    expect(db.size).toBe(1);
+    window.localStorage.setItem(`${prefix}-b`, 'b');
+    expect(db.size).toBe(2);
+  })
+
+  test('has maxAge', async () => {
+    instance.set('maxAge', '', 5);
+    await delay(10);
+    expect(instance.size).toBe(0);
+  })
+
+  test('same prefix', () => {
+    instance.set('same prefix', '');
+    const db = new LightStorage(PREFIX);
+    expect(db.size).toEqual(instance.size);
   })
 })
